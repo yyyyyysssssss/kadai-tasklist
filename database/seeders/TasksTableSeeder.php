a6c807b0ba122d51f2eb461d5a08954d4f7b1b90
yyy.sss <okkginkg@gmail.com>
2023-11-14 01:04:06 +0000

    add title

---
+++ database/seeders/TasksTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i <= 100; $i++) {
+            DB::table('tasks')->insert([
+                'status' => 'status' . $i,
+                'content' => 'test content ' . $i
+            ]);
+        }
+    }
+}
+
