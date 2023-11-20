4858b312761a98354f23846c370806434defed68
yyy.sss <okkginkg@gmail.com>
2023-11-20 06:10:18 +0000

    laravel breeze daisyui

--- database/migrations/2023_11_12_022752_add_title_to_tasks_table.php
+++ database/migrations/2023_11_12_022752_add_title_to_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status', 10);
+            $table->string('status', 10)->nullable();
         });
     }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/typography"), require("daisyui")], 
+};
